e4c2fdeaf4add9e1dcedc829295b449972f632bd
chaton <thomaschaton84@gmail.com>
2018-05-21 23:08:09 +0000

    'Binary Search Matrix'

--- code_prep/short_phone_interview_problems/binary_search_matrix.py
+++ code_prep/short_phone_interview_problems/binary_search_matrix.py
@@ -6,15 +6,12 @@ Python Implementation : Binary Search Matrix
 
 __author__ = "tchaton"
 
-'''TO FINISH'''
-
-import numpy as np
 
 def test():
-    matrix = np.array([[0, 1, 2], [3, 4, 5], [6, 7, 8]])
-    assert binary_search_matrix(matrix.tolist(), 1) == (0, 1)
-    assert binary_search_matrix(matrix.tolist(), -1) == None
-    assert binary_search_matrix(matrix.tolist(), 8) == (2, 2)
+    matrix = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
+    assert binary_search_matrix(matrix, 1) == (0, 1)
+    assert binary_search_matrix(matrix, -1) == None
+    assert binary_search_matrix(matrix, 8) == (2, 2)
 
 def binary_search_matrix(m, value):
     rows = len(m)
