cb320ac47839fb6272cc22cd91ff24976d4244ae
chaton <thomaschaton84@gmail.com>
2018-06-09 15:15:38 +0000

    'Peak and Valleys'

---
+++ code_prep/searching_and_sorting/sort/sort_peaks_valleys.py
@@ -0,0 +1,29 @@
+# Reorder an array into peaks and valleys.
+
+__author__ = 'tchaton'
+
+def peaks_and_valleys(array):
+    if len(array) < 3:
+        return
+    for index in range(len(array) - 1):
+        if index % 2:
+            if array[index] < array[index + 1]:
+                array[index], array[index + 1] = array[index + 1], array[index]
+        else:
+            if array[index] > array[index + 1]:
+                array[index], array[index + 1] = array[index + 1], array[index]
+
+
+import unittest
+
+class Test(unittest.TestCase):
+    def test_peaks_and_valleys(self):
+        a = [12, 6, 3, 1, 0, 14, 13, 20, 22, 10]
+        peaks_and_valleys(a)
+        self.assertEqual(a, [6, 12, 1, 3, 0, 14, 13, 22, 10, 20])
+        b = [34, 55, 60, 65, 70, 75, 85, 10, 5, 16]
+        peaks_and_valleys(b)
+        self.assertEqual(b, [34, 60, 55, 70, 65, 85, 10, 75, 5, 16])
+
+if __name__ == "__main__":
+    unittest.main()
