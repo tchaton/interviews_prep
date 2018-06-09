d02820694aa08d56587ff6c024ae4bbd8a2eec52
chaton <thomaschaton84@gmail.com>
2018-06-09 12:56:09 +0000

    'Add set and list cleaning'

--- code_prep/short_phone_interview_problems/permuations_without_dups.py
+++ code_prep/short_phone_interview_problems/permuations_without_dups.py
@@ -1,24 +1,27 @@
 # List all permutations of a string that contains no duplicate letters.
+def permutation(str):
+    def partial_permutations(str):
+        if len(str) < 2:
+            return [str]
 
-def partial_permutations(str):
-    if len(str) < 2:
-        return [str]
-
-    result = []
-    for index, letter in enumerate(str):
-        new_str = str[:index] + str[index+1:]
-        for perm in partial_permutations(new_str):
-            result.append(letter+perm)
-    return result
+        result = []
+        for index, letter in enumerate(str):
+            new_str = str[:index] + str[index+1:]
+            for perm in partial_permutations(new_str):
+                result.append(letter+perm)
+        return result
+    return list(set(partial_permutations(str)))
 
 import unittest
 
 class Test(unittest.TestCase):
     def test_permutations(self):
-        self.assertEqual(partial_permutations("ABCD"), ["ABCD", "ABDC", "ACBD", "ACDB",
+        self.assertEqual(permutation("ABCD"), list(set(["ABCD", "ABDC", "ACBD", "ACDB",
             "ADBC", "ADCB", "BACD", "BADC", "BCAD", "BCDA", "BDAC", "BDCA",
             "CABD", "CADB", "CBAD", "CBDA", "CDAB", "CDBA", "DABC", "DACB",
-            "DBAC", "DBCA", "DCAB", "DCBA"])
+            "DBAC", "DBCA", "DCAB", "DCBA"])))
+        self.assertEqual(permutation("abca"), list(set(["aabc", "aacb", "abac", "abca",
+    "acab", "acba", "baac", "baca", "bcaa", "caab", "caba", "cbaa"])))
 
 if __name__ == "__main__":
     unittest.main()
